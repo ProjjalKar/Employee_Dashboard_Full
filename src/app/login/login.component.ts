14d72ff9b21d86786f8ca04c420469898e66dd55
ProjjalKar <projjalkar@gmail.com>
2025-05-13 04:50:46 +0530

    source code

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  username = '';
+  password = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onSubmit() {
+    if (this.authService.login(this.username, this.password)) {
+      this.router.navigate(['/dashboard']);
+    } else {
+      alert('Invalid login');
+    }
+  }
+}
